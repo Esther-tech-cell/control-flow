35a25bc087467301496f3343ccd2a5ad8d4e0107
Esther-tech-cell <niffysbm@gmail.com>
2025-06-01 23:12:40 +0100

    Add files via upload

---
+++ weather_case.py
@@ -0,0 +1,14 @@
+Python 2.7.6 (default, Nov 10 2013, 19:24:18) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> weather = input("What's the weather like today? (sunny/rainy/cold): ")
+
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
+    
+What's the weather like today? (sunny/rainy/cold): 
